1d0493287ce4dcc27bb748e459ed26302b568785
ali <ali@pardus>
2025-01-10 15:37:34 +0300

    logging

--- source/update_client_ca.py
+++ source/update_client_ca.py
@@ -6,11 +6,16 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 
+from .logger_utils import get_logger
+logger = get_logger(__name__)
+
 def update_client_ca(server_uri, output_path):
 
     context = ssl.create_default_context()
     context.check_hostname = False
     context.verify_mode = ssl.CERT_NONE
+    
+    logger.info("Client CA updated...")
 
     with socket.create_connection((server_uri, 443)) as sock:
         with context.wrap_socket(sock, server_hostname=server_uri) as ssock:
